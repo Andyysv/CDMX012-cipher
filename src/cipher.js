b6c31d471d26ec342cf7a46878e8647bf86287ef
GiselaR <anagiselareyesruiz.gmail.com>
2022-02-01 13:14:58 -0600

    cifrado uno

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,46 @@
 const cipher = {
-  // ...
-};
+  encode: (offset, msnU) => {
+    //let alphabet= "abcdefghijklmnopqrstwyz".split("");
+    //let cipherBox = document.getElementById("cipherBox");
+    console.log(offset, msnU)
+    let cipherMsn = '';
+    //ciclo de string 
+    for (let a = 0; a < msnU.length; a++) {
+      console.log(msnU[a])
+      let letraAscii = msnU.charCodeAt(a);
+      console.log(letraAscii)
+      if (letraAscii >= 65 && letraAscii <= 90) {
+        let cifradoCesar = ((letraAscii - 65 + parseInt(offset)) % 26 + 65);
+        let nuevaLetra = String.fromCharCode(cifradoCesar);
+        cipherMsn += nuevaLetra;
+        console.log(String.fromCharCode(cifradoCesar));
+      } else if (letraAscii >= 97 && letraAscii <= 122) {
+        let cifradoCesar = ((letraAscii - 97 + parseInt(offset)) % 26 + 97);
+        let nuevaLetra = String.fromCharCode(cifradoCesar);
+        cipherMsn += nuevaLetra;
+        console.log(String.fromCharCode(cifradoCesar));
+      }
+    } console.log(cipherMsn); return cipherMsn;
+  },
 
+  decode: function (offset, msnU) {
+    let cipherMsn = '';
+    for (let a = 0; a < msnU.length; a++) {
+      let letraAscii = msnU.charCodeAt(a);
+      if (letraAscii >= 65 && letraAscii <= 90) {
+        let cifradoCesar2 = ((letraAscii - 90 - parseInt(offset)) % 26 + 90);
+        let nuevaLetra = String.fromCharCode(cifradoCesar2);
+        cipherMsn += nuevaLetra;
+        console.log(cipherMsn);
+      } else if (letraAscii >= 97 && letraAscii <= 122) {
+        let cifradoCesar2 = ((letraAscii - 97 - parseInt(offset)) % 26 + 97);
+        let nuevaLetra = String.fromCharCode(cifradoCesar2);
+        cipherMsn += nuevaLetra;
+        console.log(cipherMsn);
+      }
+    }
+    return cipherMsn;
+  }
+
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
+//let msnU= document.getElementById("cipherBox").value;
+// let offset= document.getElementById ("offset").value;
 
-console.log(cipher);
+     //let desactivatedButton= document.getElementbyId ("desactivatedButton");
+     //let descipherBox= document.getElementById("descipherBox");
+     //function boton{
+         // alert ()
+     //}
+
+     // cifrado
+     document.getElementById ("activatedButton").addEventListener("click", function(){
+      let msnU = document.getElementById ("cipherBox").value;
+       let offset = document.getElementById ("offset").value;
+        let caja = document.getElementById ("caja");   
+        caja.innerHTML = "Tu texto es: "  + cipher.encode (offset, msnU); 
+        })
+     // descifrado
+     document.getElementById ("desactivatedButton").addEventListener("click", function(){
+        let msnU = document.getElementById ("cipherBox").value;
+        let offset = document.getElementById ("offset").value;
+        let caja2= document.getElementById ("caja2");
+       caja2.innerHTML = "Tu texto es: "  + cipher.decode (offset, msnU); 
+        })
+     //let cipherBox = document.getElementById("cipherBox").value;
+    //console.log (cipherBox.toUpperCase())
+    //})
+   
+//función del boton de descifrado
+//desactivatedButton.addEventListener("click", function(){
+     //let descipherBox = document.getElementById("descipherBox").value;
+    //console.log (descipherBox)
+    //})
+ 
+   
+
